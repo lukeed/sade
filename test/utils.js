98079db71c1453241ed9970ff4ddc4a3fa020a56
Luke Edwards <luke.edwards05@gmail.com>
2018-02-07 14:39:28 -0800

    add `utils.sentences` tests

--- test/utils.js
+++ test/utils.js
@@ -19,3 +19,16 @@ test('utils.parse', t => {
 	});
 	t.end();
 });
+
+test('utils.sentences', t => {
+	[
+		['foo bar', ['foo bar']], // no . or cap
+		['foo. bar', ['foo. bar']], // no capital
+		['foo. Bar', ['foo.', 'Bar']], // has capital
+		['I haz $125.00 money. Hello', ['I haz $125.00 money.', 'Hello']],
+		['Hello.     World!', ['Hello.', 'World!']] // trims
+	].forEach(arr => {
+		t.same($.sentences(arr[0]), arr[1], `(${arr[0]}) ~~> [${arr[1]}]`);
+	});
+	t.end();
+});
